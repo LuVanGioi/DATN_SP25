239d6553bb2f93d2e9bc2b1b7b53ad94ec482b00
LuVanGioi <gioilvph47449@fpt.edu.vn>
2025-02-19 14:54:31 +0700

    test

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -11,7 +11,7 @@ class adminController extends Controller
      */
     public function index()
     {
-        return view("admins.index"); #hihi
+        return view("admins.index"); #heheeh
     }
 
     /**
