b528b4367a169c10fec38b09294daf98113f9029
Duc luu <luuquangduc04012001@gmail.com>
2025-02-19 14:52:39 +0700

    demo

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -11,7 +11,7 @@ class adminController extends Controller
      */
     public function index()
     {
-        return view("admins.index");////////
+        return view("admins.index");#gigigi
     }
 
     /**
