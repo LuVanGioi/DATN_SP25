53e1155af7920dfb2c3669818dbb51db071126b3
Duc luu <luuquangduc04012001@gmail.com>
2025-02-23 13:55:40 +0700

    Đức sửa lại xung đột

--- database/migrations/2025_02_21_152753_create_ma_giam_gia_table.php
+++ database/migrations/2025_02_21_152753_create_ma_giam_gia_table.php
@@ -10,7 +10,6 @@ class CreateMaGiamGiaTable extends Migration
     {
         Schema::create('maGiamGia', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD
             $table->string('name'); 
             $table->integer('value'); 
             $table->integer('min_value'); 
@@ -19,19 +18,8 @@ class CreateMaGiamGiaTable extends Migration
             $table->date('start_date'); 
             $table->date('end_date'); 
             $table->string('status'); 
-            $table->integer('quantity')->default(0)->after('name');
-=======
-            $table->string('name'); // Tên mã
-            $table->integer('value'); // Giá trị
-            $table->integer('min_value'); // Giá trị tối thiểu
-            $table->integer('max_value'); // Giá trị tối đa
-            $table->string('condition'); // Điều kiện
-            $table->date('start_date'); // Ngày bắt đầu
-            $table->date('end_date'); // Ngày kết thúc
-            $table->string('status'); // Trạng thái
-            $table->string('Xoa')->default(0);
-            $table->string('deleted_at')->nullable();
->>>>>>> ced845091a229786836a2b68dc19c9b2803e6504
+            $table->integer('quantity')->default(0); 
+            $table->softDeletes(); 
             $table->timestamps();
         });
     }
