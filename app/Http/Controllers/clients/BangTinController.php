63a73275f4f53b5ea94b07d1500d7c4ce1eee1ef
longlb47324 <lebaolong127ss@gmail.com>
2025-03-29 02:28:39 +0700

    fix

--- app/Http/Controllers/clients/BangTinController.php
+++ app/Http/Controllers/clients/BangTinController.php
@@ -19,7 +19,7 @@ class BangTinController extends Controller
     $newsList = DB::table("bai_viet")
         ->where("Xoa", 0)
         ->orderByDesc("id")
-        ->paginate(2); // Hiển thị 5 bài mỗi trang
+        ->paginate(2);
 
     return view("clients.BaiViet.Baiviet", compact("newsList"));
 }
@@ -30,6 +30,7 @@ class BangTinController extends Controller
     public function show($id)
      {
         $chiTiet = DB::table('bai_viet')->find($id);
+
         return view("clients.BaiViet.Chitiet", compact("chiTiet"));
      }
 }
