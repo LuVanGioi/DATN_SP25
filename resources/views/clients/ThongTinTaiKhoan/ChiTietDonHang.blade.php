0301855a877b2853187ac92c61eae118369b97d0
Lư Văn Giỏi <gioilvph47449@fpt.edu.vn>
2025-04-02 00:19:14 +0700

    FIx Giao Diện Lịch Sử Đơn Hàng - Clients

--- resources/views/clients/ThongTinTaiKhoan/ChiTietDonHang.blade.php
+++ resources/views/clients/ThongTinTaiKhoan/ChiTietDonHang.blade.php
@@ -1,97 +1,79 @@
 @extends("clients.themes")
 
 @section("titleHead")
-    <title>Chi Tiết Đơn Hàng</title>
+<title>Chi Tiết Đơn Hàng</title>
 @endsection
 
 @section('main')
-   
-    <section class="page-section">
-        <div class="wrap container">
-            <div class="row">
-                <div class="col-lg-9 col-md-9 col-sm-8">
-                    <div class="information-title">Chi Tiết Đơn Hàng Của Bạn </div>
-                    <div class="details-wrap">
-                        <div class="block-title alt"> <i class="fa fa-angle-down"></i>Đơn Hàng <strong
-                                class="pull-right">Mã Đơn Hàng: #{{$chiTietDonHang->MaDonHang}}</strong></div>
-                     
-                           
-                                <div class="row">
-                                    <div class="col-md-12 col-sm-6">
-                                        <span style="color:#111111; font-weight: bold;">Người Nhận : {{$chiTietDonHang->HoTen}}</span>                                      
-                                    </div>
 
-                                    <div class="col-md-12 col-sm-6">
-                                        <span style="color:#111111; font-weight: bold;">Số Điện Thoại : {{$chiTietDonHang->SoDienThoai}}</span>
-                                    </div>
+<section class="page-section">
+    <div class="container">
+        <div class="row mt-1">
+            <div class="col-lg-9 col-md-9 col-sm-8">
+                <div class="information-title">Chi Tiết Đơn Hàng Của Bạn </div>
+                <div class="details-wrap">
+                    <div class="block-title alt"><a href="{{ route("lich-su-don-hang.index") }}"><i class="fa fa-angle-left"></i></a> Đơn Hàng
+                        <strong class="pull-right">Mã Đơn Hàng: #{{$chiTietDonHang->MaDonHang}} | <?= trangThaiDonHang($chiTietDonHang->TrangThaiDonHang); ?></strong>
+                    </div>
 
-                                    <div class="col-md-12  col-sm-6">
-                                        <span style="color:#111111; font-weight: bold;">Địa Chỉ Nhận : {{$chiTietDonHang->DiaChi . ", " . $chiTietDonHang->Xa . " - " . $chiTietDonHang->TenHuyen . " - " . $chiTietDonHang->TenThanhPho}}</span>
-                                    </div>
-
-                                    <div class="col-md-12 col-sm-6">
-                                        <span style="color:#111111; font-weight: bold;">Ngày Mua : {{$chiTietDonHang->created_at}}</span>
-                                    </div>
-
-                                    <div class="col-md-12 col-sm-6">
-                                        <span style="color:#111111; font-weight: bold;">Trạng Thái Đơn : <?=trangThaiDonHang($chiTietDonHang->TrangThaiDonHang);?></span>
-                                    </div>
-
-                                    <div class="col-md-12 col-sm-6">
-                                        <span style="color:#111111; font-weight: bold;">Phương Thức Thanh Toán : {{$chiTietDonHang->PhuongThucThanhToan}}</span>
-                                    </div>
-
-                                    <div class="col-md-12  col-sm-6">
-                                        <span style="color:#111111; font-weight: bold;">Giảm Giá : {{$chiTietDonHang->GiamGia}}đ</span>
-                                    </div>
-
-                                    <div class="col-md-12  col-sm-6">
-                                        <span style="color:#111111; font-weight: bold;">Tổng Tiền : {{$chiTietDonHang->TongTien}}đ</span>
-                                    </div>
-                                </div>
-                </div>
-
-                    <div class="card shadow-lg">
-                        <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
-                        </div>
+                    <div class="card">
                         <div class="card-body">
-                            <div class="row mt-3">
-                                @foreach ($sanPhamMua as $item) 
-                                <div class="col-xxl-4 col-md-12">
-                                    <div class="prooduct-details-box" 
-                                        style="display: flex; align-items: center; border: 1px solid #ddd; padding: 10px; border-radius: 8px; background: #fff;">
-                                        <div class="media" style="display: flex; align-items: center; gap: 15px; width: 100%;">
-                                            <img class="align-self-center img-fluid img-60" width="100" height="100" 
-                                                src="{{ Storage::url($item->HinhAnh)}}" alt="#" 
-                                                style="object-fit: cover; border-radius: 5px;">
-                                            <div class="media-body ms-3" 
-                                                style="display: flex; flex-direction: column; align-items: flex-start; width: 100%; color: black;">
-                                                <div class="product-name" style="margin-bottom: 5px; color: black;">
-                                                    <h6 style="margin: 0; font-size: 16px; font-weight: bold; color: black;">
-                                                        <a href="/san-pham/{{ $item->DuongDan }}" target="_blank" style="color: black; text-decoration: none;">
-                                                            {{ $item->TenSanPham }}
-                                                        </a>
-                                                    </h6>
-                                                </div>
-                                                <div class="text" style="font-size: 14px; margin-bottom: 5px; color: black; ; font-weight: bold">
-                                                    {{ $item->KichCo }} - {{ $item->MauSac }}
-                                                </div>
-                                                <div class="price d-flex" style="font-weight: bold; font-size: 14px; margin-bottom: 5px; color: black;">
-                                                    <div class="text" style="color: black;">Giá : </div>{{ number_format($item->GiaTien, 0, ',', '.') }}đ
-                                                </div>
-                                                <div class="avaiabilty" style="font-size: 14px; color: black;">
-                                                    <div class="text" style="color: black;; font-weight: bold">Số Lượng : {{ $item->SoLuong }}</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                               
-                                @endforeach
+                            <div class="deltail-order">
+                                <span>Người Nhận: {{$chiTietDonHang->HoTen}}</span>
+                                <span>Số Điện Thoại : {{$chiTietDonHang->SoDienThoai}}</span>
+                                <span style="width: 100%">Địa Chỉ Nhận : {{$chiTietDonHang->DiaChi . ", " . $chiTietDonHang->Xa . " - " . $chiTietDonHang->TenHuyen . " - " . $chiTietDonHang->TenThanhPho}}</span>
                             </div>
                         </div>
                     </div>
-                
+
+                </div>
+
+                <div class="card">
+                    <div class="card-body">
+                        <h4>SẢN PHẨM</h4>
+                        <hr>
+                        @foreach ($sanPhamMua as $item)
+                        <div class="card card-body">
+                            <div class="media" style="display: flex; align-items: center; gap: 15px; width: 100%; padding: 10px 5px">
+                                <img class="align-self-center img-fluid img-60" width="100" height="100"
+                                    src="{{ Storage::url($item->HinhAnh)}}" alt="#"
+                                    style="object-fit: cover; border-radius: 5px;">
+                                <div class="media-body ms-3"
+                                    style="display: flex; flex-direction: column; align-items: flex-start; width: 100%; color: black;">
+                                    <div class="product-name" style="margin-bottom: 5px; color: black;">
+                                        <h6 style="margin: 0; font-size: 16px; font-weight: bold; color: black;">
+                                            <a href="/san-pham/{{ $item->DuongDan }}" target="_blank" style="color: black; text-decoration: none;">
+                                                {{ $item->TenSanPham }}
+                                            </a>
+                                        </h6>
+                                    </div>
+                                    <div class="text" style="font-size: 13px; margin-bottom: 5px; color: #888;">
+                                        {{ $item->KichCo }} - {{ $item->MauSac }}
+                                    </div>
+                                    <div class="price d-flex" style="font-size: 14px; margin-bottom: 5px; color: black;">
+                                        <div class="text" style="color: black;">Giá : </div>{{ number_format($item->GiaTien, 0, ',', '.') }}đ
+                                    </div>
+                                    <div class="avaiabilty" style="font-size: 14px; color: black;">
+                                        <div class="text" style="color: black;">Số Lượng : {{ $item->SoLuong }}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        @endforeach
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-body">
+                        <div class="deltail-total-order">
+                            <div><span>Giảm Giá:</span> <span>₫{{ number_format($chiTietDonHang->GiamGia) }}</span></div>
+                            <div><span>Thành Tiền : </span> <span style="color: #010660">₫{{ number_format($chiTietDonHang->TongTien) }}</span></div>
+                            <div><span>Phương Thức Thanh Toán :</span> <span style="font-weight: 500; font-size: 15px">{{$chiTietDonHang->PhuongThucThanhToan}}</span></div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
 
 
@@ -109,6 +91,6 @@
                 </div>
             </div>
         </div>
-        </div>
-    </section>
+    </div>
+</section>
 @endsection
--- resources/views/clients/ThongTinTaiKhoan/LichSuDonHang.blade.php
+++ resources/views/clients/ThongTinTaiKhoan/LichSuDonHang.blade.php
@@ -1,65 +1,75 @@
+<?php
+
+use Illuminate\Support\Facades\Storage;
+?>
 @extends("clients.themes")
 
 @section("title")
-    <title>Lịch sử đơn hàng - WD-14</title>
+<title>Lịch sử đơn hàng - WD-14</title>
 @endsection
 
 @section('main')
-    <section class="page-section">
-        <div class="wrap container">
-            <div class="row">
-                <div class="col-md-9">
-                    <div class="information-title">Lịch Sử Đơn Hàng Của Bạn</div>
-                    <div class="details-wrap">
-                        <div class="details-box orders">
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th>Hình Ảnh</th>
-                                        <th>Số Lượng</th>
-                                        <th>Tên Sản Phẩm</th>
-                                        <th>Giá Tiền</th>
-                                        <th>Mã Đơn Hàng</th>
-                                        <th>Ngày Mua</th>
-                                        <th>Trạng Thái</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($lichSu  as $item)
-                                    <tr>
-                                        <td class="image"><img width="100px" height="100px" src="{{ Storage::url($item->HinhAnh)}}"></td>
-                                        <td class="text">{{$item->SoLuong}}</td>
-                                        <td class="description"><h4><a href="/san-pham/{{ $item->DuongDan }}" target="_blank">{{ $item->TenSanPham }}</a></td>
-                                        <td class="text">{{$item->GiaTien}}</td>
-                                        <td class="text">{{$item->MaDonHang}} </td>
-                                        <td class="text"> {{$item->created_at}} </td>
-                                        <td class="" ><?=trangThaiDonHang($item->TrangThaiDonHang);?> </td>
-                                        <td class="order-status">
-                                            <a href="{{route('lich-su-don-hang.show', $item->MaDonHang)  }}" class="btn btn-theme btn-theme-dark">Chi Tiết</a> 
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                    
-                                </tbody>
-                            </table>
+<section class="page-section">
+    <div class="wrap container">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="information-title">Lịch Sử Đơn Hàng Của Bạn</div>
+                <div class="list-order-client">
+                    @foreach ($lichSu as $item)
+                    <div class="item-order-client">
+                        <div class="item-header">
+                            <span>WanderWeave</span>
+                            <span><?= trangThaiDonHang($item->TrangThaiDonHang); ?></span>
+                        </div>
+                        @foreach (DB::table('san_pham_don_hang')
+                        ->join('san_pham', 'san_pham_don_hang.Id_SanPham', '=', 'san_pham.id')
+                        ->where('MaDonHang', $item->MaDonHang)->get() as $sanPhamDonHang)
+                        <div class="item-product">
+                            <div class="img-product">
+                                <img src="<?= Storage::url($sanPhamDonHang->HinhAnh); ?>" alt="">
+                            </div>
+                            <div class="info-product">
+                                <span class="name-product">{{ $sanPhamDonHang->TenSanPham }}</span>
+                                <span class="classify-product">Phân Loại Hàng: {{ $sanPhamDonHang->KichCo }} - {{ $sanPhamDonHang->MauSac }}</span>
+                                <span class="amount-product">x{{ $sanPhamDonHang->SoLuong }}</span>
+                            </div>
+                            <div class="prices-product">
+                                <span><del>₫{{ number_format($sanPhamDonHang->GiaTien) }}</del></span>
+                                <span>₫{{ number_format($sanPhamDonHang->GiaSanPham) }}</span>
+                            </div>
+                        </div>
+                        @endforeach
+                        <div class="item-footer">
+                            <div class="total-item">Thành Tiền: <span>₫{{ number_format($item->TongTien) }}</span></div>
+                            <div class="note">
+                                <span>Đã Hủy Bởi Bạn</span>
+                            </div>
+                            <div class="list-button">
+                                <button class="btn btn-theme btn-vip">Mua Lại</button>
+                                <a class="btn btn-theme btn-donHang">Xem Chi Tiết Hủy Đơn</a>
+                                <a class="btn btn-theme btn-donHang">Đánh Giá</a>
+                                <a class="btn btn-theme btn-donHang">Xem Đánh Giá</a>
+                                <a href="{{ route("lich-su-don-hang.show", $item->MaDonHang) }}" class="btn btn-theme btn-donHang">Chi Tiết</a>
+                            </div>
                         </div>
                     </div>
+                    @endforeach
                 </div>
+            </div>
 
-                <div class="col-lg-3 col-md-3 col-sm-4">
-                    <div class="widget account-details">
-                        <h2 class="widget-title">Tài Khoản</h2>
-                        <ul>
-                            <li><a href="/thong-tin-tai-khoan"> Thông Tin Tài Khoản </a></li>
-                            <li class="active"><a href="/doi-mat-khau">Đổi Mật Khẩu</a></li>
-                            <li><a href="/lich-su-don-hang">Lịch Sử Đơn Hàng</a></li>
-                            <li><a href="/danh-gia-va-nhan-xet">Đánh Giá và Nhận Xét</a></li>
-                            <li><a href="/yeu-cau-tra-hang">Yêu Cầu Trả Hàng</a></li>
-                        </ul>
-                    </div>
+            <div class="col-lg-3 col-md-3 col-sm-4">
+                <div class="widget account-details">
+                    <h2 class="widget-title">Tài Khoản</h2>
+                    <ul>
+                        <li><a href="/thong-tin-tai-khoan"> Thông Tin Tài Khoản </a></li>
+                        <li class="active"><a href="/doi-mat-khau">Đổi Mật Khẩu</a></li>
+                        <li><a href="/lich-su-don-hang">Lịch Sử Đơn Hàng</a></li>
+                        <li><a href="/danh-gia-va-nhan-xet">Đánh Giá và Nhận Xét</a></li>
+                        <li><a href="/yeu-cau-tra-hang">Yêu Cầu Trả Hàng</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
