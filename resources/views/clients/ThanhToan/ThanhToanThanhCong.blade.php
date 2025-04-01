3cac58a8b8d06a29206d6e87d59146ed537d29d3
Lư Văn Giỏi <gioilvph47449@fpt.edu.vn>
2025-04-02 00:40:48 +0700

    Hoàn Thiện Thanh Toán Online - Clients

--- resources/views/clients/ThanhToan/ThanhToanThanhCong.blade.php
+++ resources/views/clients/ThanhToan/ThanhToanThanhCong.blade.php
@@ -11,7 +11,7 @@
             <div class="col-md-12">
                 <div class="invoice">
                     <div class="icon"><i class="fas fa-check"></i></div>
-                    <div class="title">Đặt Hàng Thành Công</div>
+                    <div class="title">Thành Công</div>
                     <div class="note">Vui lòng chú ý Email và Số điện thoại để nhận thông báo về đơn hàng</div>
                     <div class="d-block mt-3"><a href="{{ route("home.client") }}" class="btn btn-theme btn-theme-dark">Quay Về Trang Chủ</a></div>
                 </div>
