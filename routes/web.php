c6c6368fe0a348eab7a1592234d52cb6b18f3474
PHP3 <vunam2372004@gmail.com>
2025-04-03 10:31:01 +0700

    TrangThaiDon

--- routes/web.php
+++ routes/web.php
@@ -38,13 +38,14 @@ use App\Http\Controllers\admins\BienTheSanPhamController;
 use App\Http\Controllers\admins\DanhMucBaiVietController;
 use App\Http\Controllers\admins\LienKetWebsiteController;
 use App\Http\Controllers\admins\ThongTinLienHeController;
+use App\Http\Controllers\clients\ChiTietHuyDonController;
 use App\Http\Controllers\clients\LichSuDonHangController;
 use App\Http\Controllers\admins\BinhLuanBaiVietController;
 use App\Http\Controllers\clients\BaiVietChiTietController;
+use App\Http\Controllers\clients\DanhMucSanPhamController;
 use App\Http\Controllers\clients\ForgotPasswordController;
 use App\Http\Controllers\clients\ThongTinTaiKhoanController;
 use App\Http\Controllers\clients\AuthController as ClientsAuthController;
-use App\Http\Controllers\clients\DanhMucSanPhamController;
 use App\Http\Controllers\clients\homeController as ClientsHomeController;
 use App\Http\Controllers\clients\supportController as ClientSupportController;
 use App\Http\Controllers\clients\SanPhamController as ClientsSanPhamController;
@@ -118,8 +119,7 @@ Route::put('doi-mat-khau/update/{id}', [DoiMatKhauController::class, 'update'])-
 
 
 Route::resource('lich-su-don-hang', LichSuDonHangController::class);
-
-
+Route::resource('chi-tiet-huy-don', ChiTietHuyDonController::class);
 Route::get('danh-muc/{code}',[DanhMucSanPhamController::class, 'show'])->name('danh-muc.show');
 
 
